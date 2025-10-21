4e146209d08fae8c51b54b68d6140f1d1b68787a
Kushal Acharya <106709757+KushalAcharya00@users.noreply.github.com>
2025-10-21 15:08:43 -0400

    userlist and hashing

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -1,47 +1,71 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import {
   Divider,
   List,
   ListItem,
   ListItemText,
   Typography,
-}
-from '@mui/material';
+} from '@mui/material';
+import fetchModel from '../../lib/fetchModelData';
 import './userList.css';
 
-/**
- * Define UserList, a React component of project #5
- */
 class UserList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      users: [],
+      loading: true,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    // Fetch users from the backend
+    fetchModel('/user/list')
+      .then((response) => {
+        this.setState({ users: response.data, loading: false });
+      })
+      .catch((err) => {
+        console.error('Error fetching user list:', err);
+        this.setState({ error: err.statusText, loading: false });
+      });
   }
 
   render() {
+    const { users, loading, error } = this.state;
+
+    if (loading) {
+      return <Typography variant="body1">Loading users...</Typography>;
+    }
+
+    if (error) {
+      return <Typography color="error">Error: {error}</Typography>;
+    }
+
     return (
-      <div>
-        <Typography variant="body1">
-          This is the user list, which takes up 3/12 of the window.
-          You might choose to use <a href="https://mui.com/components/lists/">Lists</a> and <a href="https://mui.com/components/dividers/">Dividers</a> to
-          display your users like so:
+      <div className="user-list-container">
+        <Typography variant="h6" sx={{ marginBottom: '8px' }}>
+          Users
         </Typography>
+
         <List component="nav">
-          <ListItem>
-            <ListItemText primary="Item #1" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #2" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #3" />
-          </ListItem>
-          <Divider />
+          {users.map((user, index) => (
+            <React.Fragment key={user._id}>
+              <ListItem
+                button
+                component={Link}
+                to={`/users/${user._id}`}
+                className="user-list-item"
+              >
+                <ListItemText
+                  primary={`${user.first_name} ${user.last_name}`}
+                />
+              </ListItem>
+              {index !== users.length - 1 && <Divider />}
+            </React.Fragment>
+          ))}
         </List>
-        <Typography variant="body1">
-          The model comes in from window.models.userListModel()
-        </Typography>
       </div>
     );
   }
