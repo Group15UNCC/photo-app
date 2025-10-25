e6b1666c92a5efc6dc8788f875c1fa04cb1c943a
Kushal Acharya <106709757+KushalAcharya00@users.noreply.github.com>
2025-10-24 22:06:18 -0400

    userphotos

--- components/userPhotos/userPhotos.jsx
+++ components/userPhotos/userPhotos.jsx
@@ -1,32 +1,124 @@
 import React from 'react';
 import {
-  Typography
+  Typography,
+  CircularProgress,
+  Card,
+  CardContent,
+  CardMedia
 } from '@mui/material';
+import fetchModel from '../../lib/fetchModelData';
 import './userPhotos.css';
 
-
 /**
- * Define UserPhotos, a React componment of project #5
+ * Define UserPhotos, a React component of project #5
  */
 class UserPhotos extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      photos: null,
+      loading: true,
+      error: null
+    };
+  }
 
+  componentDidMount() {
+    this.loadUserPhotos();
+  }
+
+  componentDidUpdate(prevProps) {
+    // If userId changes (navigating between users), refetch
+    if (prevProps.match.params.userId !== this.props.match.params.userId) {
+      this.loadUserPhotos();
+    }
+  }
+
+  loadUserPhotos() {
+    const userId = this.props.match.params.userId;
+    this.setState({ loading: true, error: null });
+
+    fetchModel(`/photosOfUser/${userId}`)
+      .then((response) => {
+        this.setState({
+          photos: response.data,
+          loading: false
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          error: `Failed to load photos: ${err.statusText}`,
+          loading: false
+        });
+      });
   }
 
   render() {
-    return (
-      <Typography variant="body1">
-      This should be the UserPhotos view of the PhotoShare app. Since
-      it is invoked from React Router the params from the route will be
-      in property match. So this should show details of user:
-      {this.props.match.params.userId}. You can fetch the model for the user from
-      window.models.photoOfUserModel(userId):
-        <Typography variant="caption">
-          {JSON.stringify(window.models.photoOfUserModel(this.props.match.params.userId))}
-        </Typography>
-      </Typography>
+    const { photos, loading, error } = this.state;
+    const userId = this.props.match.params.userId;
 
+    if (loading) {
+      return (
+        <div className="userPhotos-container">
+          <CircularProgress />
+          <Typography variant="body1" sx={{ mt: 1 }}>
+            Loading photos for user {userId}...
+          </Typography>
+        </div>
+      );
+    }
+
+    if (error) {
+      return (
+        <Typography color="error" variant="body1">
+          {error}
+        </Typography>
+      );
+    }
+
+    if (!photos || photos.length === 0) {
+      return (
+        <Typography variant="body1">
+          No photos found for user {userId}.
+        </Typography>
+      );
+    }
+
+    return (
+      <div className="userPhotos-container">
+        <Typography variant="h6" gutterBottom>
+          Photos of User {userId}
+        </Typography>
+
+        {photos.map((photo) => (
+          <Card key={photo._id} className="userPhotos-card">
+            {photo.file_name && (
+              <CardMedia
+                component="img"
+                image={`../images/${photo.file_name}`}
+                alt={photo.file_name}
+                className="userPhotos-image"
+              />
+            )}
+            <CardContent>
+              <Typography variant="body2" color="textSecondary">
+                Uploaded on: {new Date(photo.date_time).toLocaleString()}
+              </Typography>
+
+              {/* Optional: render comments if available */}
+              {photo.comments && photo.comments.length > 0 && (
+                <div className="photo-comments">
+                  <Typography variant="subtitle2">Comments:</Typography>
+                  {photo.comments.map((comment) => (
+                    <Typography key={comment._id} variant="body2">
+                      <strong>{comment.user?.first_name}</strong>: {comment.comment}
+                    </Typography>
+                  ))}
+                </div>
+              )}
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     );
   }
 }
