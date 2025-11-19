46aa63e16079535232d9eb25259bf928e71166ca
Kushal Acharya <106709757+KushalAcharya00@users.noreply.github.com>
2025-11-18 21:15:42 -0500

    photo uploading ui changes

--- components/topBar/TopBar.jsx
+++ components/topBar/TopBar.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import {
-    AppBar, Toolbar, Typography
+    AppBar, Toolbar, Typography, Button
 } from '@mui/material';
 import './TopBar.css';
 import axios from 'axios';
 
-/**
- * Define TopBar, a React componment of project #5
- */
 class TopBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             app_info: undefined
         };
+        this.uploadInput = null;
     }
 
     componentDidMount() {
@@ -33,19 +31,63 @@ class TopBar extends React.Component {
         }
     }
 
-  render() {
-    return this.state.app_info ? (
-      <AppBar className="topbar-appBar" position="absolute">
-        <Toolbar>
-            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>Todd Dobbs</Typography>
-            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }} color="inherit">{this.props.main_content}</Typography>
-            <Typography variant="h5" component="div" color="inherit">Version: {this.state.app_info.version}</Typography>
-        </Toolbar>
-      </AppBar>
-    ) : (
-        <div/>
-    );
-  }
+    handleUploadButtonClicked = (e) => {
+        e.preventDefault();
+        if (this.uploadInput && this.uploadInput.files.length > 0) {
+            const domForm = new FormData();
+            domForm.append("uploadedphoto", this.uploadInput.files[0]);
+
+            axios.post("/photos/new", domForm)
+                .then((res) => {
+                    console.log("Upload success:", res.data);
+                })
+                .catch((err) => {
+                    console.error("Upload error:", err);
+                });
+        }
+    };
+
+    render() {
+        return this.state.app_info ? (
+            <AppBar className="topbar-appBar" position="absolute">
+                <Toolbar>
+
+                    <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>Brown University</Typography>
+                    <Typography variant="h5" component="div" sx={{ flexGrow: 1 }} color="inherit">{this.props.main_content}</Typography>
+
+                    {/* ----- Hidden File Input ----- */}
+                    <input
+                        type="file"
+                        accept="image/*"
+                        ref={(ref) => { this.uploadInput = ref; }}
+                        style={{ display: "none" }}
+                    />
+
+                    {/* ----- Add Photo Button ----- */}
+                    <Button
+                        color="inherit"
+                        onClick={() => this.uploadInput && this.uploadInput.click()}
+                        sx={{ marginRight: "16px" }}
+                    >
+                        Add Photo
+                    </Button>
+
+                    {/* ----- Upload Button ----- */}
+                    <Button
+                        color="inherit"
+                        onClick={this.handleUploadButtonClicked}
+                        sx={{ marginRight: "16px" }}
+                    >
+                        Upload
+                    </Button>
+
+                    <Typography variant="h5" component="div" color="inherit">Version: {this.state.app_info.version}</Typography>
+                </Toolbar>
+            </AppBar>
+        ) : (
+            <div />
+        );
+    }
 }
 
 export default TopBar;
